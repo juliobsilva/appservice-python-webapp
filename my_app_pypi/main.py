2477fc68304ab987887a41c77ce4ff630b723ec4
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-07-05 09:37:33 -0300

    Fix setup

--- my_app_pypi/main.py
+++ my_app_pypi/main.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return "Olá, esta é a sua mensagem exibida na página web!"
 
-def main():
+if __name__ == '__main__':
     app.run(debug=True)
